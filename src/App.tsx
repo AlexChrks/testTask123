ec537452da0d4e97f933e8a73b20411fefa3d7c5
EMozheiko <e.mozheiko@softteco.com>
2024-10-17 17:38:47 +0400

    add question creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import "./App.css";
 
+import { Box, Container, Stack } from "@mui/system";
+import { Input, Button, InputLabel, List, ListItem, ListItemText } from "@mui/material";
+
 type Question = {
 	text: string;
 	options: string[];
@@ -12,6 +15,9 @@ function App() {
 
 	const [newQuestionName, setNewQuestionName] = useState("");
 	const [newOptionName, senNewOptionName] = useState("");
+
+	const isAddOptionDisabled = !Boolean(newOptionName);
+	const isAddQuestionDisabled = !Boolean(newQuestionName) || !Boolean(newQuestionOptions.length);
 
 	const handleQuestionTextChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
 		const { value } = target;
@@ -34,39 +40,66 @@ function App() {
 			{ text: newQuestionName, options: [...newQuestionOptions] }
 		]);
 		setNewQuestionName("");
+		setNewQuestionOptions([]);
+	};
+
+	const removeAnswerOption = (optionIndex: number) => () => {
+		const newOptions = [...newQuestionOptions];
+		newOptions.splice(optionIndex, 1);
+
+		setNewQuestionOptions([...newOptions]);
 	};
 
 	return (
-		<>
-			<div>
-				{questions?.map(({ text, options }) => (
-					<div>
-						<div key={text}>{text}</div>
-						<div>
-							{options?.map((option) => (
-								<span>{option}</span>
-							))}
-						</div>
-					</div>
-				))}
-			</div>
-			<div>
-				<input
+		<Container>
+			<Stack>
+				<InputLabel>Question text:</InputLabel>
+				<Input
 					value={newQuestionName}
 					placeholder="Question text"
 					onChange={handleQuestionTextChange}
 				/>
 
-				<input
+				<List dense>
+					<ListItemText primary="Answer options:" />
+					{newQuestionOptions.map((option, index) => (
+						<ListItem key={option}>
+							<ListItemText primary={option} />
+							<Button variant="text" onClick={removeAnswerOption(index)}>
+								Remove
+							</Button>
+						</ListItem>
+					))}
+				</List>
+
+				<Input
 					value={newOptionName}
 					placeholder="Answer option text"
 					onChange={handleOptionTextChange}
 				/>
-				<button onClick={addOption}>Add answer option</button>
+				<Button disabled={isAddOptionDisabled} variant="outlined" onClick={addOption}>
+					Add answer option
+				</Button>
+			</Stack>
 
-				<button onClick={addQuestion}>Add question</button>
-			</div>
-		</>
+			<Button disabled={isAddQuestionDisabled} variant="contained" onClick={addQuestion}>
+				Add question
+			</Button>
+
+			<Stack spacing="sm">
+				{questions?.map(({ text, options }) => (
+					<Box key={text}>
+						<h3>Question: {text}</h3>
+						<h4>Answers:</h4>
+						<List>
+							{options?.map((option) => (
+								<ListItem>{option}</ListItem>
+							))}
+						</List>
+					</Box>
+				))}
+			</Stack>
+		</Container>
 	);
 }
 
